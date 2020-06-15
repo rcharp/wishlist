9452a5d80b3cd2bc256dc466b7454c942b288c77
ricky <rickycharpentier@gmail.com>
2020-06-15 00:19:22 -0400

    changes

--- app/app.py
+++ app/app.py
@@ -366,7 +366,9 @@ def percent_filter(arg):
 
 
 def any_filter(arg, k):
-    return any(x.feedback_id == k for x in arg)
+    if next((x for x in arg if x.feedback_id == k), None) is not None:
+        return True
+    return False
 
 
 def default_profile_image_url(arg):
