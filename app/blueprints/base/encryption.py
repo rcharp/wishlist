be721918e106e29e772679815d7551f54c569899
ricky <rickycharpentier@gmail.com>
2020-06-22 14:13:57 -0400

    changes

--- app/blueprints/base/encryption.py
+++ app/blueprints/base/encryption.py
@@ -3,12 +3,12 @@ from simplecrypt import encrypt, decrypt
 
 
 def encrypt_string(plaintext):
-    key = os.environ.get('SECRET_KEY')
+    key = 'test'
     ciphertext = encrypt(key, plaintext)
     return ciphertext
 
 
 def decrypt_string(cipher):
-    key = os.environ.get('SECRET_KEY')
+    key = 'test'
     plaintext = decrypt(key, cipher)
     return plaintext
