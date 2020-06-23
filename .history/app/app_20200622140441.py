803fd10481e559837a2c3b72fb9c25a495a2a3e7
ricky <rickycharpentier@gmail.com>
2020-06-23 02:17:40 -0400

    changes

--- .history/app/app_20200622140441.py
+++ .history/app/app_20200622140441.py
@@ -90,14 +90,6 @@ def create_app(settings_override=None):
         # Set the app server name
         app.config['SERVER_NAME'] = 'getwishlist.io'
         app.config['REMEMBER_COOKIE_DOMAIN'] = '.getwishlist.io'
-
-        # Set the http -> https redirect
-        @app.before_request
-        def before_request():
-            if not request.is_secure:
-                url = request.url.replace("http://", "https://", 1)
-                code = 301
-                return redirect(url, code=code)
     else:
         # Set the app server name
         app.config['SERVER_NAME'] = 'localhost:5000'
