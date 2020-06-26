4f1c004be6aa7dfe3dbe60aa10d99b819bbfe338
ricky <rickycharpentier@gmail.com>
2020-06-26 16:25:39 -0400

    changes

--- app/blueprints/base/functions.py
+++ app/blueprints/base/functions.py
@@ -181,6 +181,7 @@ def update_comment(c, content):
 def format_comments(comments, current_user):
 
     try:
+        print(current_user)
         comment_list = list()
 
         for comment in comments:
