0830071bc0cc8e049b1172ed476377e26b616aa2
ricky <rickycharpentier@gmail.com>
2020-07-07 12:16:07 -0400

    changes

---
+++ push.py
@@ -0,0 +1,11 @@
+import os
+os.system("git checkout dev")
+os.system("git add .")
+os.system('git commit -m "changes')
+os.system('git push origin dev')
+os.system("git checkout master")
+os.system("git pull origin dev --force")
+os.system("git add .")
+os.system('git commit -m "push to master"')
+os.system("git push origin master")
+os.system("git checkout dev")
