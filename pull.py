30c178606622d5a2abd7f67bb70a5b402630c5e5
ricky <rickycharpentier@gmail.com>
2020-08-13 07:43:52 -0400

    changes

--- pull.py
+++ pull.py
@@ -1,2 +1,6 @@
 import os
+os.system("git checkout dev")
+os.system('git pull origin dev --force')
+os.system("git checkout master")
 os.system('git pull origin master --force')
+os.system("git checkout dev")
